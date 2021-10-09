73bbb5cb4a6832c1ed822185db9c835089356d80
Vadim <gabderahmanov99@gmail.com>
2021-10-09 21:02:52 +0300

    add savedata for save in preference

---
+++ app/src/main/java/com/vadim/termometr/utils/SaveData.java
@@ -0,0 +1,41 @@
+package com.vadim.termometr.utils;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SaveData {
+    private SharedPreferences sPref;
+    private Context context;
+    public SaveData(Context context) {
+        this.context=context;
+    }
+
+    //save check state
+    public void saveState(boolean isCheck) {
+        sPref = context.getSharedPreferences("save_state", MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putBoolean("isChecked", isCheck);
+        ed.apply();
+    }
+
+    public boolean loadState() {
+        sPref = context.getSharedPreferences("save_state", MODE_PRIVATE);
+        return sPref.getBoolean("isChecked", true);
+    }
+
+    //save changed farengete or celestial from menu
+    public void saveChangedTypeTemperature(boolean isCheckTypeTemperature) {
+        sPref = context.getSharedPreferences("save_change_type", MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        ed.putBoolean("isCheckTypeTemperature", isCheckTypeTemperature);
+        ed.apply();
+    }
+
+    public boolean loadChangedTypeTemperature() {
+        sPref = context.getSharedPreferences("save_change_type", MODE_PRIVATE);
+        return sPref.getBoolean("isCheckTypeTemperature", true);
+    }
+}

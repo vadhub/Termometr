8040c6867ae3bcccfff42dfdce120b3e126cb720
Vadim <gabderahmanov99@gmail.com>
2021-10-14 11:23:13 +0300

    call directly from TemperatureFromPath

--- app/src/main/java/com/vadim/termometr/screens/main/TemperPresentor.java
+++ app/src/main/java/com/vadim/termometr/screens/main/TemperPresentor.java
@@ -1,18 +1,20 @@
 package com.vadim.termometr.screens.main;
 
 import com.vadim.termometr.R;
+import com.vadim.termometr.temperprocessor.TemperatureFromPath;
+import com.vadim.termometr.utils.Convertor;
 
 public class TemperPresentor {
 
     private TemperatureView view;
-    private TemperatureProcessor temperatureProcessor = new TemperatureProcessor();
+    private TemperatureFromPath temperature = new TemperatureFromPath();
 
     public TemperPresentor(TemperatureView view) {
         this.view=view;
     }
 
-    public void setTemperature() {
-        float t = temperatureProcessor.getTemperatureCPU();
+    public void getTemperature() {
+        float t = Convertor.temperatureHuman(temperature.getTemperature());
         if (t==-100) {
             view.showError(R.string.warning);
         }

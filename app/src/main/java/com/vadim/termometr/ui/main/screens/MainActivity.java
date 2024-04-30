3ae61d1701b6c024bbd519b9d398323515565084
vadim <gabderahmanov99@gmail.com>
2024-04-30 13:16:25 +0300

    fix: permission post notification not required

--- app/src/main/java/com/vadim/termometr/ui/main/screens/MainActivity.java
+++ app/src/main/java/com/vadim/termometr/ui/main/screens/MainActivity.java
@@ -1,21 +1,28 @@
 package com.vadim.termometr.ui.main.screens;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
+import android.util.DisplayMetrics;
+import android.widget.Magnifier;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.vadim.termometr.R;
 import com.vadim.termometr.ui.main.servicetemper.ServiceBackgroundTemperature;
 import com.vadim.termometr.ui.main.temperatureview.Thermometer;
-import com.yandex.mobile.ads.banner.AdSize;
+import com.yandex.mobile.ads.banner.BannerAdSize;
 import com.yandex.mobile.ads.banner.BannerAdView;
 import com.yandex.mobile.ads.common.AdRequest;
 
@@ -56,9 +63,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BannerAdView mBanner = (BannerAdView) findViewById(R.id.adView);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requirePermission();
+        }
+
         //R-M-DEMO-320x50
         mBanner.setAdUnitId("R-M-1993169-1");
-        mBanner.setAdSize(AdSize.stickySize(AdSize.FULL_SCREEN.getWidth()));
+        mBanner.setAdSize(getAdSize(mBanner));
         AdRequest adRequest = new AdRequest.Builder().build();
         mBanner.loadAd(adRequest);
 
@@ -77,6 +89,26 @@ public class MainActivity extends AppCompatActivity {
                 serviceBackgroundTemperature.cleanedNotification();
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+    private void requirePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.POST_NOTIFICATIONS}, 0);
+        }
+    }
+
+    private BannerAdSize getAdSize(BannerAdView mBanner ) {
+        final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        // Calculate the width of the ad, taking into account the padding in the ad container.
+        int adWidthPixels = mBanner.getWidth();
+        if (adWidthPixels == 0) {
+            // If the ad hasn't been laid out, default to the full screen width
+            adWidthPixels = displayMetrics.widthPixels;
+        }
+        final int adWidth = Math.round(adWidthPixels / displayMetrics.density);
+
+        return BannerAdSize.stickySize(this, adWidth);
     }
 
     void doUnbindService() {
